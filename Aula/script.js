9725af46667c63ea217931e8bcda21aeea938ef1
Davi-Prisma-Aluno00 <davivenancioprisma@gmail.com>
2026-02-10 09:00:52 -0300

    Add files via upload

---
+++ Aula/script.js
@@ -0,0 +1,36 @@
+class Barcelona{
+  
+    constructor(nome,gols,vitorias,derrotas,empates){
+
+this.none = none
+    this.gols = gols
+    this.vitorias = vitorias
+    this.derrotas = derrotas
+    this.empates = empates
+  }
+  calcularPontos (vitorias){
+    this.vitorias += vitorias
+  }
+  
+  calcularSaldo(vitorias){
+    this.vitorias += vitorias
+  }
+
+   registrarFimdejogo(vitorias){
+           this.vitorias = vitorias
+
+
+   }
+    
+
+
+
+
+
+
+}
+ let campeonato = new Barcelona("Barcelona",99,1,0)
+ Barcelona.vitorias = 99
+ Barcelona.derrotas = 1
+ Barcelona.empates = 0
+  console.log(Barcelona)
